7fc14cb2e18cdad7192dfcb4c6bfc6eb394a9d10
Eli Fatsi <eli.fatsi@viget.com>
2013-11-26 16:33:02 +0000

    capture.py set up to take image, canny analyze it, and post to heroku

--- canny-car-detect.py
+++ canny-car-detect.py
@@ -1,45 +1,59 @@
 from SimpleCV import *
+import sys
+import requests
+import json
 
-car = Image("sample_data/new-in2.jpg")
-no_car = Image("sample_data/new-out3.jpg")
+if len(sys.argv) > 1:
+  image_string = sys.argv[1]
+else:
+  image_string = "sample_data/new-in2.jpg"
+image = Image(image_string)
 
 # crop with full car & no back wall:
-car = car.crop(170, 170, 230, 300)
-no_car = no_car.crop(170, 170, 230, 300)
+image = image.crop(170, 170, 230, 300)
 
 # used to use 100, 400 (was the worst)
 # followed by 50, 200 which was terrible
 # used 50, 400 with success
 # used 25, 400 with success
 # used 300, 400 with success
-car = car.edges(25, 400)
-no_car = no_car.edges(25, 400)
+image = image.edges(25, 400)
 
 # make MASK!
-mask = Image(car.size())
-dl = DrawingLayer(car.size())
+mask = Image(image.size())
+dl = DrawingLayer(image.size())
 # get rid of bushes
-dl.polygon([(230, 100), (230, 300), (0, 300), (0, 260)], filled=True, color=Color.WHITE)
+dl.polygon([(230, 100), (230, 300), (0, 300), (0, 200)], filled=True, color=Color.WHITE)
 # get rid of brick wall
 dl.polygon([(0, 0), (35, 0), (10, 300), (0, 300)], filled=True, color=Color.WHITE)
 # get rid of back of car
-dl.polygon([(115, 0), (230, 0), (230, 300), (115, 300)], filled=True, color=Color.WHITE)
+#dl.polygon([(115, 0), (230, 0), (230, 300), (115, 300)], filled=True, color=Color.WHITE)
 mask.addDrawingLayer(dl)
 mask = mask.applyLayers()
 
-car = car - mask
-no_car = no_car - mask
+image = image - mask
 
-result = car.sideBySide(no_car)
-result.show()
+#image.show()
 
-car_matrix = car.getNumpy().flatten()
-no_car_matrix = no_car.getNumpy().flatten()
+image_matrix = image.getNumpy().flatten()
 
-car_pixel_count = cv2.countNonZero(car_matrix)
-no_car_pixel_count = cv2.countNonZero(no_car_matrix)
+image_pixel_count = cv2.countNonZero(image_matrix)
 
-print "Car image has " + str(car_pixel_count) + " pixels"
-print "No Car image has " + str(no_car_pixel_count) + " pixels"
+#print "Image " + image_string + " has " + str(image_pixel_count) + " pixels"
 
-raw_input()
+url = "http://pi-parking.herokuapp.com/updates"
+files = {'update[image]': (image_string, open(image_string, 'rb'))}
+#headers = {'content-type': 'image/png'}
+
+if image_pixel_count > 4000:
+  print "TAKEN"
+  status = {'update[status]': 'taken'}
+else:
+  print "AVAILABLE"
+  status = {'update[status]': 'available'}
+
+#r = requests.post(url, data=status, files=files, headers=headers)
+r = requests.post(url, data=status, files=files)
+print r.text
+
+#raw_input()
--- capture.py
+++ capture.py
@@ -1,16 +1,79 @@
-import pygame
-import pygame.camera
-from pygame.locals import *
+#import pygame
+#import pygame.camera
+#from pygame.locals import *
+
+#pygame.init()
+#pygame.camera.init()
+
+#cam = pygame.camera.Camera("/dev/video0",(640,480))
+#cam.start()
+#image = cam.get_image()
+
+#fileName = "screenshot_" + datetime.now().strftime("%m-%d_%H:%M") + ".png"
+
+#pygame.image.save(image, fileName)
+
+
+from SimpleCV import *
+import sys
+import requests
+import json
 from datetime import datetime
 
-pygame.init()
-pygame.camera.init()
+cam = Camera()
 
-cam = pygame.camera.Camera("/dev/video0",(640,480))
-cam.start()
-image = cam.get_image()
+image = cam.getImage()
+fileName = "screenshot_" + datetime.now().strftime("%m-%d_%H:%M") + ".png"
 
-fileName = "screenshot_" + datetime.now().strftime("%m-%d_%H:%M")
+image.save(fileName)
 
-pygame.image.save(image, fileName)
+# crop with full car & no back wall:
+image = image.crop(170, 170, 230, 300)
 
+# used to use 100, 400 (was the worst)
+# followed by 50, 200 which was terrible
+# used 50, 400 with success
+# used 25, 400 with success
+# used 300, 400 with success
+image = image.edges(25, 400)
+
+# make MASK!
+mask = Image(image.size())
+dl = DrawingLayer(image.size())
+# get rid of bushes
+dl.polygon([(230, 100), (230, 300), (0, 300), (0, 200)], filled=True, color=Color.WHITE)
+# get rid of brick wall
+dl.polygon([(0, 0), (35, 0), (10, 300), (0, 300)], filled=True, color=Color.WHITE)
+# get rid of back of car
+#dl.polygon([(115, 0), (230, 0), (230, 300), (115, 300)], filled=True, color=Color.WHITE)
+mask.addDrawingLayer(dl)
+mask = mask.applyLayers()
+
+image = image - mask
+
+#image.show()
+
+image_matrix = image.getNumpy().flatten()
+
+image_pixel_count = cv2.countNonZero(image_matrix)
+
+#print "Image " + fileName + " has " + str(image_pixel_count) + " pixels"
+
+image.save("canny-" + fileName)
+
+url = "http://pi-parking.herokuapp.com/updates"
+files = {'update[image]': (fileName, open(fileName, 'rb')), 'update[canny_image]': ("canny-" + fileName, open("canny-" + fileName, 'rb'))}
+#headers = {'content-type': 'image/png'}
+
+if image_pixel_count > 1000:
+  print "TAKEN"
+  status = {'update[status]': 'taken', 'update[pixel_count]': image_pixel_count}
+else:
+  print "AVAILABLE"
+  status = {'update[status]': 'available', 'update[pixel_count]': image_pixel_count}
+
+#r = requests.post(url, data=status, files=files, headers=headers)
+r = requests.post(url, data=status, files=files)
+print r.text
+
+#raw_input()
